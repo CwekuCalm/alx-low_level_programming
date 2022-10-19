cd7f524f4f1515a1dd8638a708fdeb332f99c6d3
CwekuCalm <ankalbc@gmail.com>
2022-10-18 21:04:03 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * main - Entry
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
@@ -14,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
