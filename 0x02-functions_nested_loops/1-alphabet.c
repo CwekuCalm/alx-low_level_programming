0977812131520e94b7b123a35b77e6857d09f8e4
CwekuCalm <ankalbc@gmail.com>
2022-10-19 13:24:54 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alpha in lowercase
- * */
+ *print_alphabet-prints the alphabet in lowercase
+ */
 void print_alphabet(void)
 {
 	char la;
-	for (la = 'a'; la <= 'z'; laa++)
+	
+	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
+
 	_putchar('\n');
+}
