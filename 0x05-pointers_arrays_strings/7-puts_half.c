ffd269d68e2e8f2423784a0d9083e60df8bb5ba8
CwekuCalm <ankalbc@gmail.com>
2022-10-24 19:07:15 -0700

    halfc

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,15 @@ void puts_half(char *str)
 {
 	int i = 0, len = 0, j;
 
-
 	while (str[i++])
 		len++;
 
 	if ((len % 2) == 0)
 		j = len / 2;
 
-	
 	else
 		j = (len + 1) / 2;
-	
-	
+
 	for (i = j; i < len; i++)
 		_putchar(str[i]);
 
